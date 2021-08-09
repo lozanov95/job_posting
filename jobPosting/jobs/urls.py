57bf2880c29242316b6b3f0a838c643feee50408
lozanov <vasillozanov1@gmail.com>
2021-08-09 21:23:46 +0300

    added index file

--- jobPosting/jobs/urls.py
+++ jobPosting/jobs/urls.py
@@ -2,10 +2,11 @@ from django.contrib.auth.decorators import login_required
 from django.urls import path, reverse_lazy
 
 from jobPosting.jobs.views import ListJobPostingsView, DetailsJobPostingView, \
-    CreateJobPostingView, DeleteJobPostingView
+    CreateJobPostingView, DeleteJobPostingView, index
 
 urlpatterns = (
-    path('', ListJobPostingsView.as_view(), name='job list'),
+    path('', index, name='index'),
+    path('list/', ListJobPostingsView.as_view(), name='job list'),
     path('create/', login_required(CreateJobPostingView.as_view(), reverse_lazy('job list')), name='job create'),
     path('details/<int:pk>', DetailsJobPostingView.as_view(), name='job details'),
     path('delete/<int:pk>', DeleteJobPostingView.as_view(), name='job delete'),
