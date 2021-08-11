28e1568be7710feed23ea95d050d3d039ae4be0f
lozanov <vasillozanov1@gmail.com>
2021-08-11 23:23:12 +0300

    added login_required to the CRUD views

--- jobPosting/jobs/urls.py
+++ jobPosting/jobs/urls.py
@@ -10,14 +10,14 @@ urlpatterns = (
     path('list/', ListJobPostingsView.as_view(), name='job list'),
     path('my_jobs', MyJobsView.as_view(), name='job my postings'),
 
-    path('create/', login_required(CreateJobPostingView.as_view(), reverse_lazy('sign in')), name='job create'),
+    path('create/', login_required(CreateJobPostingView.as_view()), name='job create'),
     path('details/<int:pk>', DetailsJobPostingView.as_view(), name='job details'),
-    path('update/<int:pk>', EditJobPostingView.as_view(), name='job edit'),
-    path('delete/<int:pk>', DeleteJobPostingView.as_view(), name='job delete'),
+    path('update/<int:pk>', login_required(EditJobPostingView.as_view()), name='job edit'),
+    path('delete/<int:pk>', login_required(DeleteJobPostingView.as_view()), name='job delete'),
 
-    path('apply/<int:pk>', SubmitApplicationView.as_view(), name='job apply'),
-    path('my_aplications/', ListMyApplicationsView.as_view(), name='my applications list'),
-    path('applicants/<int:pk>', ListApplicantsView.as_view(), name='applications list'),
+    path('apply/<int:pk>', login_required(SubmitApplicationView.as_view()), name='job apply'),
+    path('my_aplications/', login_required(ListMyApplicationsView.as_view()), name='my applications list'),
+    path('applicants/<int:pk>', login_required(ListApplicantsView.as_view()), name='applications list'),
 
     path('success/', success_view, name='success'),
 )
