970c7992da19975f3e0dc75ac0f6485d7180af6d
lozanov <vasillozanov1@gmail.com>
2021-08-14 00:57:03 +0300

    minor refactor

--- jobPosting/job_apply/urls.py
+++ jobPosting/job_apply/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 
 from jobPosting.job_apply.views import SubmitApplicationView, ListMyApplicationsView, ListApplicantsView
 
-urlpatterns=(
+urlpatterns = (
     path('apply/<int:pk>', login_required(SubmitApplicationView.as_view()), name='job apply'),
     path('my_aplications/', login_required(ListMyApplicationsView.as_view()), name='my applications list'),
     path('applicants/<int:pk>', login_required(ListApplicantsView.as_view()), name='applications list'),
