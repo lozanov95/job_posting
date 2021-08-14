11f4d4b2ce37b3886c3115b7eb28b8910db3a1d1
lozanov <vasillozanov1@gmail.com>
2021-08-14 13:07:39 +0300

    added styling to ProfileForm

--- jobPosting/profiles/forms.py
+++ jobPosting/profiles/forms.py
@@ -7,6 +7,9 @@ class ProfileForm(forms.ModelForm):
     """
     Form used for displaying/editing the profile information.
     """
+    first_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    age = forms.IntegerField(widget=forms.NumberInput(attrs={'class': 'form-control'}))
 
     class Meta:
         model = Profile
